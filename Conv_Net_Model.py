fd4431777aa2ce2cca25d137e285e917ed5865fd
willtop <billcuitop@gmail.com>
2019-02-19 12:52:16 -0500

    Update Conv_Net_Model.py

--- Conv_Net_Model.py
+++ Conv_Net_Model.py
@@ -1,6 +1,15 @@
-# Wei Cui Dec 17th 2018
-# Spatial Deep Learning for Wireless Scheduling
-# Conv Net Structure
+# This repository contains our novel convolutional neural network model code implementation 
+# for the work "Spatial Deep Learning for Wireless Scheduling", 
+# available at https://arxiv.org/abs/1808.01486.
+
+# For any reproduce, further research or development, please kindly cite our Arxiv paper: 
+# @misc{spatial_learning18, 
+#       author = "W. Cui and K. Shen and W. Yu", 
+#       title = "Spatial Deep Learning for Wireless Scheduling", 
+#       month = dec, 
+#       year = 2018, 
+#       note = {[Online] Available: https://arxiv.org/abs/1808.01486} 
+# }
 
 import random
 import numpy as np
